caf8b56316b288d76ba189f9f6ba543244ee48be
Keegan McAllister <kmcallister@mozilla.com>
2013-12-09 17:08:57 -0800

    Add Rust test crate file

---
+++ translator-src/rust-output/lib.rs
@@ -0,0 +1,11 @@
+#[feature(managed_boxes)];
+
+pub mod AttributeName;
+pub mod ElementName;
+pub mod HtmlAttributes;
+pub mod MetaScanner;
+pub mod StackNode;
+pub mod StateSnapshot;
+pub mod Tokenizer;
+pub mod TreeBuilder;
+pub mod UTF16Buffer;
